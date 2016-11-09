5ffe94a992e567ff5b676360e4ad903bc65faf37
TB <t.bourrely@openmailbox.org>
2016-11-09 17:29:49 +0100

    systeme

--- docker_container.sh
+++ docker_container.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+
+fun(){
+	if [[ -d $1 ]]; then
+		cmd="docker run -ti --rm -v ${1}:/${2} kali-linux-full /bin/bash"
+		eval $cmd
+	else
+		echo "${1} n'est pas une repertoire valide"
+	fi
+}
+
 if [[ $# -eq 2 ]]; then
 	dirt=$1
 	dirn=$2
-	if [[ -d ${dirt} ]]; then
-		cmd="docker run -ti --rm -v ${dirt}:/${dirn} kali-linux-full /bin/bash"
-		eval $cmd
-	else
-		echo "${dirt} n'est pas une repertoire valide"
-	fi	
+	fun $dirt $dirn
+elif [[ $# -eq 1 ]]; then
+	if [[ $1="systeme" ]]; then
+		fun "/Volumes/TBOURRELYCO/Cours/S3/Systeme/M3101-systeme-s3/Exercices/" "systeme"
+	fi
 fi
