f676026b63c279b7baad5660e63debab716b73ee
TB <t.bourrely@openmailbox.org>
2016-11-23 19:12:15 +0100

    blackbox

--- docker_container.sh
+++ docker_container.sh
@@ -14,7 +14,11 @@ if [[ $# -eq 2 ]]; then
 	dirn=$2
 	fun $dirt $dirn
 elif [[ $# -eq 1 ]]; then
-	if [[ $1="systeme" ]]; then
-		fun "/Volumes/TBOURRELYCO/Cours/S3/Systeme/M3101-systeme-s3/Exercices/" "systeme"
+	if [[ $1 == "systeme" ]]; then
+		fun "/Volumes/TB_COURS/Cours/S3/Systeme/M3101-systeme-s3/Exercices/" "systeme"
+	elif [[ $1 == "blackbox" ]]; then
+		ip=$(ifconfig en0 | grep "inet " | cut -d ' ' -f 2)
+		cmd="docker run -ti --rm --name blackbox -e DISPLAY=${ip}:0 -v /tmp/.X11-unix:/tmp/.X11-unix kali-linux-full /bin/bash"
+		eval $cmd
 	fi
 fi
