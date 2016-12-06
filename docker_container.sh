14e072b3a8acc610bde2dc33fc8ea4df01ed8731
TB <t.bourrely@openmailbox.org>
2016-12-06 11:15:47 +0100

    suppression kali

--- docker_container.sh
+++ docker_container.sh
@@ -2,7 +2,7 @@
 
 fun(){
 	if [[ -d $1 ]]; then
-		cmd="docker run -ti --rm -v ${1}:/${2} kali-linux-full /bin/bash"
+		cmd="docker run -ti --rm -v ${1}:/${2} debian:jessie"
 		eval $cmd
 	else
 		echo "${1} n'est pas une repertoire valide"
@@ -16,9 +16,9 @@ if [[ $# -eq 2 ]]; then
 elif [[ $# -eq 1 ]]; then
 	if [[ $1 == "systeme" ]]; then
 		fun "/Volumes/TB_COURS/Cours/S3/Systeme/M3101-systeme-s3/Exercices/" "systeme"
-	elif [[ $1 == "blackbox" ]]; then
-		ip=$(ifconfig en0 | grep "inet " | cut -d ' ' -f 2)
-		cmd="docker run -ti --rm --name blackbox -e DISPLAY=${ip}:0 -v /tmp/.X11-unix:/tmp/.X11-unix kali-linux-full /bin/bash"
-		eval $cmd
+	#elif [[ $1 == "blackbox" ]]; then
+	#	ip=$(ifconfig en0 | grep "inet " | cut -d ' ' -f 2)
+	#	cmd="docker run -ti --rm --name blackbox -e DISPLAY=${ip}:0 -v /tmp/.X11-unix:/tmp/.X11-unix kali-linux-full /bin/bash"
+	#	eval $cmd
 	fi
 fi
